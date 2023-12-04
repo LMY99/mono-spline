d1feb69a1e4ce632d57868f2ad478adf4291e07d
LMY99 <alfie_my@hotmail.com>
2023-12-03 21:28:14 -0500

    Change to logistic truth and 10-df spline

--- main_S.R
+++ main_S.R
@@ -51,7 +51,7 @@ for(i in 1:N){
   ))
 }
 df <- df[dplyr::between(df$ageori,0,120),]
-dfi <- 8
+dfi <- 14
 qknot <- (1:(dfi-3))/(dfi-2)
 VIF <- 0.1
 boundary.knot <- c(0,120)
@@ -77,7 +77,7 @@ Y <- Y + truthRE[df$id,]
 coef00 <- c(0,0,c(1,4,7,1)/100,0,0)
 B00 <- splines2::ibs(df$ageori,knots=knot,degree=2,intercept=TRUE,Boundary.knots=c(0,120))
 
-Y[,1] <- Y[,1] + B00 %*% coef00
+Y[,1] <- Y[,1] + f_sigmoid(df$ageori,2,70,5) #B00 %*% coef00
 Y[,2] <- Y[,2] + f_sshape(df$ageori,mode1,range_L1,range_R1)
 Y[,3] <- Y[,3] + f_wiggle(df$ageori,mean1,sd1,mean2,sd2,p1,p2)
 colnames(Y) <- c('Y1','Y2','Y3')
@@ -99,7 +99,7 @@ K <- 1 # Number of biomarkers
 X <- as.matrix(df[,c('intercept')],ncol=1) 
 Y <- as.matrix(df[,c('Y')],ncol=1) # Biomarkers array
 t <- df$ageori # Age in original scale
-dfi <- 8 # DoF of Spline
+dfi <- 14 # DoF of Spline
 qknot <- (1:(dfi-3))/(dfi-2) # Quantiles to determine knots
 VIF <- 0.1 # Variance inflation factor for BETAKDE
 
@@ -266,13 +266,13 @@ indice <- seq(Burnin+1,R,1)
 spline.basis <- splines2::ibs(pmin(pmax(ages,min(boundary.knot)),max(boundary.knot)), 
                               knots=knot.list[[1]], Boundary.knots = boundary.knot, 
                               degree=2, intercept=TRUE)
-spline.basis <- spline.basis[,3:6]
+spline.basis <- spline.basis[,3:(dfi-2)]
 points <- spline.basis %*% coefs[-(1),1,indice]
 est <- apply(points,1,function(x) c(mean(x),
                                     coda::HPDinterval(coda::as.mcmc(x))))
 est <- data.frame(t(est))
 colnames(est) <- c("avg","lower","upper")
-est$truth <- spline.basis %*% coef00[3:6]
+est$truth <- f_sigmoid(ages,2,70,5)#spline.basis %*% coef00[3:6]
 est$age <- ages
 
 CI_repeat[di,,] <- as.matrix(est)
