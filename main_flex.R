7bca7553b2f76ddba4dbe53e1c18eea43373ce2d
LMY99 <alfie_my@hotmail.com>
2023-12-13 02:45:16 -0500

    Use 4-df spline model

--- main_flex.R
+++ main_flex.R
@@ -33,7 +33,7 @@ CI_repeat <- array(0,dim=c(dataset_num,1201,5))
 # coef_repeat <- array(0,dim=c(dataset_num,5000,5))
 CI_covariate_repeat <- array(0,dim=c(dataset_num,nrow(true_fixed_effect),4))
 
-coef_repeat_flex <- array(0,dim=c(dataset_num,5000,4+20))
+coef_repeat_flex <- array(0,dim=c(dataset_num,5000,4+4))
 
 for(di in 1:dataset_num){
 
@@ -51,7 +51,7 @@ for(i in 1:N){
   ))
 }
 df <- df[dplyr::between(df$ageori,0,120),]
-dfi <- 24
+dfi <- 8
 qknot <- (1:(dfi-3))/(dfi-2)
 VIF <- 0.1
 boundary.knot <- c(0,120)
@@ -78,7 +78,7 @@ Y <- Y + truthRE[df$id,]
 coef00 <- c(0,0,c(1,4,7,1)/100,0,0)
 B00 <- splines2::ibs(df$ageori,knots=knot,degree=2,intercept=TRUE,Boundary.knots=c(0,120))
 
-Y[,1] <- Y[,1] + f_sigmoid(df$ageori,2,70,5) #B00 %*% coef00
+Y[,1] <- Y[,1] + B00 %*% coef00
 Y[,2] <- Y[,2] + f_sshape(df$ageori,mode1,range_L1,range_R1)
 Y[,3] <- Y[,3] + f_wiggle(df$ageori,mean1,sd1,mean2,sd2,p1,p2)
 colnames(Y) <- c('Y1','Y2','Y3')
@@ -104,7 +104,7 @@ X <- as.matrix(df[,X_names],ncol=nX)
 
 Y <- as.matrix(df[,c('Y')],ncol=1) # Biomarkers array
 t <- df$ageori # Age in original scale
-dfi <- 24 # DoF of Spline
+dfi <- 8 # DoF of Spline
 qknot <- (1:(dfi-3))/(dfi-2) # Quantiles to determine knots
 VIF <- 0.1 # Variance inflation factor for BETAKDE
 
@@ -263,7 +263,7 @@ est <- apply(points,1,function(x) c(mean(x),
                                     coda::HPDinterval(coda::as.mcmc(x))))
 est <- data.frame(t(est))
 colnames(est) <- c("avg","lower","upper")
-est$truth <- f_sigmoid(ages,2,70,5)#spline.basis %*% coef00[3:6]
+est$truth <- spline.basis %*% coef00[3:6]
 est$age <- ages
 
 CI_repeat[di,,] <- as.matrix(est)
