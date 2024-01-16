dc8237c2fbd5ced64beeb1187417d078ff6f2ff6
LMY99 <alfie_my@hotmail.com>
2024-01-15 22:21:22 -0500

    Increase simulation size to 10

--- main_S.R
+++ main_S.R
@@ -34,7 +34,7 @@ mean1 <- 70; sd1 <- 5; mean2 <- 100; sd2 <- 5; p1 <- 0.4; p2 <- 0.6
 
 N <- 250
 
-dataset_num <- 1
+dataset_num <- 10
 
 CI_repeat <- array(0,dim=c(dataset_num,1201,6))
 turning <- array(0,dim=c(dataset_num,3))
